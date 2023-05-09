0e3155ba7060a2b238a51ea21860a9836f79f26c
Murran <claytoo.serophina@gmail.com>
2023-05-09 17:21:22 +0700

    Add Fire Extinguisher Physics

---
+++ Assets/_POJOK MURRAN/Scripts/Extinguisher.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Extinguisher : MonoBehaviour
+{
+    private void Update()
+    {
+        //if (Physics.Raycast(Camera.main.transform.position, Camera.main.forward))
+    }
+}
