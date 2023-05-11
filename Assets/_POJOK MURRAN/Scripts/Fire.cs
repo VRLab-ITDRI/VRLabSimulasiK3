385b07c2ee47b7ec8959efde5e37b9a624dd6c61
Murran <claytoo.serophina@gmail.com>
2023-05-11 23:49:50 +0700

    Update Pin

--- Assets/_POJOK MURRAN/Scripts/Fire.cs
+++ Assets/_POJOK MURRAN/Scripts/Fire.cs
@@ -7,8 +7,13 @@ public class Fire : MonoBehaviour
     [SerializeField, Range(0f, 1f)] private float currentIntensity = 1.0f;
     
     private float[] startIntensities = new float[0];
+    float timeLastWatered = 0;
+    [SerializeField] float regenDelay = 2.5f;
+    [SerializeField] float regenRate = .1f;
 
     [SerializeField] private ParticleSystem[] fireParticleSystems = new ParticleSystem[0];
+
+    private bool isLit = true;
 
     private void Start()
     {
@@ -22,7 +27,27 @@ public class Fire : MonoBehaviour
 
     private void Update()
     {
-        ChangeIntensity();
+        //ChangeIntensity();
+        if (isLit && currentIntensity < 1.0f && Time.time - timeLastWatered >= regenDelay)
+        {
+            currentIntensity += regenDelay * Time.deltaTime;
+            ChangeIntensity();
+        }
+    }
+
+    public bool TryExtinguish(float amount)
+    {
+        timeLastWatered = Time.time;
+
+        currentIntensity -= amount;
+
+        if (currentIntensity <= 0)
+        {
+            isLit = false;
+            return true;
+        }
+
+        return false;
     }
 
     private void ChangeIntensity()
