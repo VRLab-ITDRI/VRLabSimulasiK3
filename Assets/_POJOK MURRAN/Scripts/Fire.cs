a948f1e435ea19cbd515d593222bc1f560b17b8c
Murran <claytoo.serophina@gmail.com>
2023-05-12 20:35:10 +0700

    Fix Audio Listener & Extinguish Fire

--- Assets/_POJOK MURRAN/Scripts/Fire.cs
+++ Assets/_POJOK MURRAN/Scripts/Fire.cs
@@ -5,57 +5,51 @@ using UnityEngine;
 public class Fire : MonoBehaviour
 {
     [SerializeField, Range(0f, 1f)] private float currentIntensity = 1.0f;
-    
     private float[] startIntensities = new float[0];
-    float timeLastWatered = 0;
-    [SerializeField] float regenDelay = 2.5f;
-    [SerializeField] float regenRate = .1f;
 
-    [SerializeField] private ParticleSystem[] fireParticleSystems = new ParticleSystem[0];
+    [SerializeField] private ParticleSystem[] firePS = new ParticleSystem[0];
 
-    private bool isLit = true;
+    public float amount = 0.5f;
+
+    //public ParticleSystem part;
+    public List<ParticleCollisionEvent> collisionEvents;
 
     private void Start()
     {
-        startIntensities = new float[fireParticleSystems.Length];
+        startIntensities = new float[firePS.Length];
 
-        for (int i = 0; i < fireParticleSystems.Length; i++)
+        for (int i = 0; i < firePS.Length; i++)
         {
-            startIntensities[i] = fireParticleSystems[i].emission.rateOverTime.constant;
+            startIntensities[i] = firePS[i].emission.rateOverTime.constant;
         }
     }
 
     private void Update()
     {
-        //ChangeIntensity();
-        if (isLit && currentIntensity < 1.0f && Time.time - timeLastWatered >= regenDelay)
+        ChangeIntensity();
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        if (other.CompareTag("Water"))
         {
-            currentIntensity += regenDelay * Time.deltaTime;
-            ChangeIntensity();
+            Debug.LogWarning("Airnya kena Api WOY!");
+            TryExtinguish();
         }
     }
 
-    public bool TryExtinguish(float amount)
+    public void TryExtinguish()
     {
-        timeLastWatered = Time.time;
-
         currentIntensity -= amount;
-
-        if (currentIntensity <= 0)
-        {
-            isLit = false;
-            return true;
-        }
-
-        return false;
     }
 
     private void ChangeIntensity()
     {
-        for (int i = 0; i < fireParticleSystems.Length; i++)
+        for (int i = 0; i < firePS.Length; i++)
         {
-            var emission = fireParticleSystems[i].emission;
+            var emission = firePS[i].emission;
             emission.rateOverTime = currentIntensity * startIntensities[i];
         }
+        
     }
 }
